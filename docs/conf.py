9ac793e8f8b0318b9ecacecd38f9df408c120ace
Chris AtLee <chris@atlee.ca>
2019-09-13 15:06:46 -0400

    Add docs for winsign.sign

--- docs/conf.py
+++ docs/conf.py
@@ -31,8 +31,11 @@ release = '1.3.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx.ext.autodoc"
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon"
 ]
+
+napoleon_google_docstring = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
